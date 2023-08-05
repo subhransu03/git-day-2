4bd77a774154d186239d965772a6e360bec78797
subhransu03 <ssb521@nist.edu>
2023-08-05 18:22:46 +0530

    second one

--- first.c
+++ first.c
@@ -1,1 +1,11 @@
 #include<stdio.h>
+int main()
+{
+    printf("ookokokoko");
+    printf("ookokokoko");
+    printf("ookokokoko");
+    printf("hoooooooo");
+    printf("hihiiiiiii");
+
+    return 0;
+}
